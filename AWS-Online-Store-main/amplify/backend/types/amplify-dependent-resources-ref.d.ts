b2d0b4de9b5b757fa4d3e650d84d8cd7f036019e
Prashanth Renati <prashanthrenati@gmail.com>
2024-01-15 00:15:35 +0530

    all files uploaded

---
+++ AWS-Online-Store-main/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,40 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "onlinestoref314c1ab": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "storage": {
+        "s3c617b63d": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "processPayment": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "createOrder": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "onlinestore": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
